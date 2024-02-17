846bc5f00b6952955f232323c09b271a659f63fe
junren.chan <junren.chan@okg.com>
2024-02-17 22:25:18 +0800

    :sparkles: Shaders part 2 WIP

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -12,7 +12,66 @@ void main() {
     // gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
     // gl_FragColor = vec4(uColor, 1.0);
 
-    vec4 textureColor = texture2D(uTexture, vUv);
-    textureColor.rgb *= vElevation * 2.0 + 0.5;
-    gl_FragColor = textureColor;
+    // vec4 textureColor = texture2D(uTexture, vUv);
+    // textureColor.rgb *= vElevation * 2.0 + 0.5;
+    // gl_FragColor = textureColor;
+
+    // Some chio rainbow gradient
+    // gl_FragColor = vec4(vUv, 1.0, 1.0);
+
+    // Another chio gradient
+    // gl_FragColor = vec4(vUv, 0.0, 1.0);
+
+    // Black and white chio gradient
+    // float strength = vUv.x;
+    // float strength = vUv.y;
+
+    // Inverting vUv.y
+    // float strength = 1.0 - vUv.y;
+
+    // Squeezing the gradient
+    // float strength = vUv.y * 10.0;
+
+    // float strength = mod(vUv.y * 10.0, 1.0);
+    // float strength = mod(vUv.x * 10.0, 1.0);
+
+    // No gradient
+    //strength = step(0.5, strength);
+    // Bigger gap
+    // strength = step(0.8, strength);
+
+    // Combining
+    // float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
+    // strength += step(0.8, mod(vUv.y * 10.0, 1.0));
+
+    // Intersections
+    // float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
+    // strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
+
+    // float strength = step(0.4, mod(vUv.x * 10.0, 1.0));
+    // strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
+    
+    // Inverted Ls 
+    // float barX = step(0.4, mod(vUv.x * 10.0, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
+    // float barY = step(0.8, mod(vUv.x * 10.0, 1.0)) * step(0.4, mod(vUv.y * 10.0, 1.0));
+    // float strength = barX + barY;
+
+    // Many crosses
+    // float barX = step(0.4, mod(vUv.x * 10.0 - 0.2, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
+    // float barY = step(0.8, mod(vUv.x * 10.0, 1.0)) * step(0.4, mod(vUv.y * 10.0 - 0.2, 1.0));
+    // float strength = barX + barY;
+
+    // Trippy
+    // float strength = abs(vUv.x - 0.5);
+    // float strength = min(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+    // float strength = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+
+    // Squares with outlines
+    // float strength = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    // Thinner square
+    float strength = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    strength *= 1.0 - step(0.25, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+
+
+    gl_FragColor = vec4(vec3(strength), 1.0);
 }
